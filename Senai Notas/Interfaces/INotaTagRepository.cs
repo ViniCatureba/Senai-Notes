604b8412a9bb578fe0bc8559aebf91404f331a75
fic <fic@NOTE06-S28>
2025-05-15 21:58:01 -0300

    interface

---
+++ Senai Notas/Interfaces/INotaTagRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using Senai_Notas.Models;
+
+namespace Senai_Notas.Interfaces
+{
+    public interface INotaTagRepository
+    {
+        // Retorna todas as relações NotaTag de forma assíncrona
+        Task<IEnumerable<NotaTag>> ListarTodosAsync();
+
+        // Busca uma relação NotaTag pelo ID de forma assíncrona
+        Task<NotaTag?> BuscarPorIdAsync(int id);
+
+        // Cadastra uma nova relação NotaTag de forma assíncrona
+        Task CadastrarAsync(NotaTag notaTag);
+
+        // Atualiza uma relação NotaTag existente de forma assíncrona
+        Task AtualizarAsync(int id, NotaTag notaTag);
+
+        // Remove uma relação NotaTag de forma assíncrona
+        Task DeletarAsync(int id);
+    }
+}
--- Senai Notas/obj/Debug/net9.0/Senai Notas.AssemblyInfo.cs
+++ Senai Notas/obj/Debug/net9.0/Senai Notas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Senai Notas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724b6862be606dea468d7b34a5be70115081e250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9003bf86178f192f530f15c3c13284718f2e9d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Senai Notas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Senai Notas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
